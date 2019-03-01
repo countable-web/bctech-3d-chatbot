73d84669e607cf3861355f912f9e88b78791e89c
Alan Luo <me@alanluo.com>
2019-02-28 19:20:35 -0500

    basic architecture set up

---
+++ homepage/README.md
@@ -0,0 +1,13 @@
+# Countable Homepage
+
+I'm going to model the world as a series of entities moving around an origin point.
+```
+entity: {
+    origin
+    init()
+    loop()
+    kill()
+}
+```
+
+This allows us to manage memory by modeling the whole thing as essentially an expanded starfield. So instead of moving the user through a giant box, we make things travel towards the user. When the entity enters the loading zone, we run `init()`. Then `loop()` every frame. After it exits the loading zone, we `kill()`.
--- homepage/js/index.js
+++ homepage/js/index.js
@@ -1,12 +1,8 @@
-console.log('loaded!');
-
-
-var clock, scene, camera, renderer, controls, stats, geometry, particles, raycaster;
-var mouse = new THREE.Vector2();
+var clock, scene, camera, renderer, controls, stats, particles;
+var mouse = {x:0, y:0};
+var arrows = {x:0, y:0};
 
 var particles = [];
-
-var noisefn = noise.simplex3;
 
 function init() {
 	stats = initStats();
@@ -14,54 +10,71 @@ function init() {
 	clock = new THREE.Clock();
 	clock.start();
 
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 5000);
-	// camera = new THREE.OrthographicCamera(window.innerWidth/-2, window.innerWidth/2, window.innerHeight/2, window.innerHeight/-2, 1, 5000);
-	camera.position.z = 1000;
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 5000);
+	//first, let's render a wireframe 1000-wide cube, where everything is going to happen.
+
+	camera.position.z = 3000;
 	camera.position.y = 1000;
-	// camera.position.z = 1000;
-	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	camera.position.x = 0;
+	camera.lookAt(0, 0, 0);
+
+
 	scene = new THREE.Scene();
 	// scene.fog = new THREE.FogExp2(0x000000, 0.001);
 	scene.add(camera);
+	
 
-	geometry = new THREE.Geometry();
+	var cubeGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
+	var cubeMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
+	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+	// scene.add(cube);
+
+	var wireframe = new THREE.WireframeGeometry(cubeGeometry);
+	var line = new THREE.LineSegments(wireframe);
+	scene.add(line);
+
+	var ambientLight = new THREE.AmbientLight( 0x000000 );
+	scene.add( ambientLight );
+
+	var lights = [];
+	lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+	lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+	lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+	lights[ 0 ].position.set( 0, 200, 0 );
+	lights[ 1 ].position.set( 100, 200, 100 );
+	lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+	scene.add( lights[ 0 ] );
+	scene.add( lights[ 1 ] );
+	scene.add( lights[ 2 ] );
+
+
+	var geometry = new THREE.Geometry();
 	sprite = new THREE.TextureLoader().load('./images/disk.png');
 	//make particles
-	var dimension = 100;
-	var increment = 2000/(dimension-1);
-	for(var i=0; i<dimension; i++) {
-		for(var j=0; j<dimension; j++) {
-			var vertex = new THREE.Vector3();
-			vertex.x = -1000+i * increment;
-			vertex.z = -1000+j * increment;
-			vertex.y = 0;
+	for(var zval=-1000; zval<1000; zval++) {
+		var vertex = new THREE.Vector3();
+		vertex.x = 2000 * Math.random() - 1000;
+		vertex.y = 2000 * Math.random() - 1000;
+		vertex.z = zval;
 
-			var norm = Math.pow(vertex.x, 2) + Math.pow(vertex.z, 2);
-			var max = (-1000)*(-1000)+(-1000)*(-1000);
-
-			geometry.vertices.push( vertex );
-			
-			var thiscolor = new THREE.Color();
-			thiscolor.setHSL(Math.sqrt(norm/max), 0.7, 0.4);
-			geometry.colors.push(thiscolor);
-			// geometry.colors.push(new THREE.Color(norm/max*0xffffff));
-		}
+		geometry.vertices.push( vertex );
+		geometry.colors.push(new THREE.Color(0xffffff));
 	}
 
 
 
-	material = new THREE.PointsMaterial({size: 50, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true, vertexColors: THREE.VertexColors} );
+	material = new THREE.PointsMaterial({size: 20, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true, vertexColors: THREE.VertexColors} );
 
 	particles = new THREE.Points(geometry, material);
 
+	// particles.
 	scene.add(particles);
-	// console.log(geometry);
+	console.log(geometry);
 
-	//renderer, raycaster
+	//render
 	renderer = new THREE.WebGLRenderer();
-	raycaster = new THREE.Raycaster();
-	raycaster.params.Points.threshold = 100; //not sure what this is
-
 
 	//finish
 	document.getElementById("WebGL-output").appendChild(renderer.domElement);
@@ -79,44 +92,56 @@ function renderScene() {
 function updateParticles() {
 	var time = clock.getElapsedTime();
 
-	//starts at x=0, z=1000
-	camera.position.x = 1000*Math.cos(time*0.2+Math.PI/2);
-	camera.position.z = 1000*Math.sin(time*0.2+Math.PI/2);
+	// camera.position.x += (  mouse.x - camera.position.x*0.6) * 0.1;
+	// camera.position.y += (- mouse.y - camera.position.y*0.6) * 0.1;
+	camera.position.y += arrows.y * 20;
+	camera.position.x += arrows.x * 20;
 
-	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	camera.lookAt(scene.position);
 
 	for(var i=0; i<particles.geometry.vertices.length; i++) {
-		currentpos = particles.geometry.vertices[i];
-		// particles.geometry.vertices[i].y=80*Math.sin(currentpos.x*0.008+0.5*time)+
-										// 80*Math.sin(currentpos.z*0.008+0.8*time);
-		var particleheight = (noisefn(currentpos.x/300, currentpos.z/300, time*0.2)+1)*70;
-		particles.geometry.vertices[i].y=particleheight;
-		particles.geometry.colors[i].setHSL(particles.geometry.vertices[i].y/160, 0.7, 0.4);
+		particles.geometry.vertices[i].z+=30;
+		if(particles.geometry.vertices[i].z>1000) {
+			particles.geometry.vertices[i].z=-1000;
+		}
 	}
 	particles.geometry.verticesNeedUpdate=true;
-	particles.geometry.colorsNeedUpdate=true;
-
-	raycaster.setFromCamera(mouse, camera);
-	var intersections = raycaster.intersectObjects();
-	// intersection = ( intersections.length ) > 0 ? intersections[ 0 ] : null;
-
-	// if(time%10<1) console.log(intersections);
 	// h = time * 0.1 % 360;
 	// material.color.setHSL(h, 0.5, 0.5);
 }
 
 
-window.addEventListener('resize', onResize, false);
+
+
 function onResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
-window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('resize', onResize, false);
 function onMouseMove( event ) {
 	mouse.x = event.clientX - window.innerWidth / 2;
 	mouse.y = event.clientY - window.innerHeight / 2;
 }
+window.addEventListener('mousemove', onMouseMove, false);
+
+function onKeydown( event ) {
+	if(event.keyCode == 38) { // up
+		arrows.y = 1;
+	} else if(event.keyCode == 39) { // right
+		arrows.x = 1;
+	} else if(event.keyCode == 40) { // down
+		arrows.y = -1;
+	} else if(event.keyCode == 37) { // left
+		arrows.x = -1;
+	}
+}
+window.addEventListener('keydown',onKeydown,false);
+function onKeyup( event ) {
+	arrows.x = 0; arrows.y = 0;
+}
+window.addEventListener('keyup',onKeyup,false);
+
 
 function initStats() {
 	var statcontroller = new Stats();
