8efbd6c3b0f0d3d8b644387fd9928ad64785216b
Alan Luo <me@alanluo.com>
2019-03-06 10:50:32 -0500

    encapsulated new curtain into PolyCurtain

---
+++ homepage/js/entity/PolyCurtain.js
@@ -0,0 +1,123 @@
+function PolyCurtain(origin, length, radius) {
+	this.origin = {x: 0, y:0, z:0};
+	this.origin.x = origin.x; 
+	this.origin.y = origin.y; 
+	this.origin.z = origin.z;
+	this.entityObj = {};
+	this.spacing = 8;
+	this.particles = [];
+	this.alive = true;
+	this.colormap = {};
+
+	for(let i=0; i<length; i++) {
+		let row = [];
+		for(let j=0; j<radius; j++) {
+			let rval = -0.5*this.spacing*(radius-1) + j * this.spacing;
+			let zval = -0.5*this.spacing*(length-1) + i * this.spacing;
+			let thetaval = zval/30;
+
+			// let xval = r * Math.sin(zval/20);
+			// let yval = r * Math.cos(zval/20);
+
+			this.particles.push({
+				r: rval, 
+				theta: thetaval,
+				z: zval,
+			});
+		}
+	}
+
+	this.init = function() {
+		// make particles
+		var geometry = new THREE.Geometry();
+		var material = new THREE.MeshNormalMaterial();
+		material.side = THREE.DoubleSide;
+		
+		let width = 10;
+		let height = 20;
+		let indices = [];
+		let index = 0;
+
+		let t_spacing = 10;
+		let t_width = t_spacing * width;
+		let t_left = -0.5*t_width;
+		let t_height = t_spacing * height;
+		let t_bottom = -0.5*t_height;
+
+		//construct vertices
+		for(i=0; i<width; i++) {
+			let row = [];
+			for(j=0; j<height; j++) {
+				geometry.vertices.push(new THREE.Vector3(i*t_spacing+t_left,0,j*t_spacing+t_bottom));
+				row.push(index);
+				index++;
+			}
+			indices.push(row);
+		}
+		//construct faces
+		for(i=0; i<width-1; i++) {
+			for(j=0; j<height-1; j++) {
+				geometry.faces.push(new THREE.Face3(indices[i+1][j+1], indices[i+1][j], indices[i][j]));
+				geometry.faces.push(new THREE.Face3(indices[i][j+1], indices[i+1][j+1], indices[i][j]));
+			}
+		}
+
+		for(i=0; i<geometry.vertices.length; i++) {
+			let myv = geometry.vertices[i];
+			// myv.x += -5+Math.random()*10;
+			// myv.z += -5+Math.random()*10;
+			let r = myv.x;
+			myv.x = r*Math.cos(myv.z/30);
+			myv.y = r*Math.sin(myv.z/30);
+			// myv.y = 10*noise.simplex3(myv.x/10, myv.z/10,0);
+		}
+
+		geometry.verticesNeedUpdate = true;
+		geometry.computeFaceNormals();
+		geometry.normalsNeedUpdate = true;
+
+		let entityObj = new THREE.Mesh(geometry, material);
+		entityObj.position.x = this.origin.x;
+		entityObj.position.y = this.origin.y;
+		entityObj.position.z = this.origin.z;
+		this.entityObj = entityObj;
+		scene.add(entityObj);
+
+	}	
+	this.loop = function(){
+		// this.entityObj.translateZ(3);
+
+		// let oldvertices = this.entityObj.geometry.vertices;
+		// let oldcolors = this.entityObj.geometry.colors;
+		// for(let i=0; i<this.particles.length; i++) {
+		// 	this.particles[i].theta+=0.01;
+		// }
+
+		// for(let i=0; i<oldvertices.length; i++) {
+		// 	oldvertices[i].x = this.particles[i].r * Math.cos(this.particles[i].theta);
+		// 	oldvertices[i].y = this.particles[i].r * Math.sin(this.particles[i].theta);
+
+		// 	this.entityObj.geometry.colors[i].offsetHSL(0.002, 0, 0);
+		// }
+		// this.entityObj.geometry.verticesNeedUpdate=true;
+		// this.entityObj.geometry.colorsNeedUpdate=true;
+
+		// if(this.entityObj.position.z > 500) {
+		// 	this.entityObj.position.z = - 400;
+		// } else if(this.entityObj.position.z < -500) {
+		// 	this.entityObj.position.z = 400;
+		// } if(this.entityObj.position.x > 500) {
+		// 	this.entityObj.position.x = -400;
+		// } else if(this.entityObj.position.x < -500) {
+		// 	this.entityObj.position.x = 400;
+		// }
+
+
+		// if(this.origin.z>700) { this.origin.z = -700; }
+		// if(this.origin.z<-700) { this.origin.z = 700; }
+	}
+	this.kill = function(){
+		scene.remove(this.entityObj);
+		this.alive = false;
+	}
+}
--- homepage/js/script/index2.js
+++ homepage/js/script/index2.js
@@ -58,73 +58,52 @@ function init() {
 
 
 	// create fabrics
-	// let myEntity = new PolyCurtain({x:0, y:0, z:0}, 30, 10);
-	// myEntity.init();
-	// entities.push(myEntity);
-	var geometry = new THREE.Geometry();
-	var material = new THREE.MeshNormalMaterial();
-	material.side = THREE.DoubleSide;
-	var mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
-
-	let width = 10;
-	let height = 20;
-	let indices = [];
-	let index = 0;
-
-	let t_spacing = 10;
-	let t_width = t_spacing * width;
-	let t_left = -0.5*t_width;
-	let t_height = t_spacing * height;
-	let t_bottom = -0.5*t_height;
-
-	//construct vertices
-	for(i=0; i<width; i++) {
-		let row = [];
-		for(j=0; j<height; j++) {
-			geometry.vertices.push(new THREE.Vector3(i*t_spacing+t_left,0,j*t_spacing+t_bottom));
-			row.push(index);
-			index++;
-		}
-		indices.push(row);
-	}
-	//construct faces
-	for(i=0; i<width-1; i++) {
-		for(j=0; j<height-1; j++) {
-			geometry.faces.push(new THREE.Face3(indices[i+1][j+1], indices[i+1][j], indices[i][j]));
-			geometry.faces.push(new THREE.Face3(indices[i][j+1], indices[i+1][j+1], indices[i][j]));
-		}
-	}
-
-	for(i=0; i<geometry.vertices.length; i++) {
-		let myv = geometry.vertices[i];
-		// myv.x += -5+Math.random()*10;
-		// myv.z += -5+Math.random()*10;
-		let r = myv.x;
-		myv.x = r*Math.cos(myv.z/30);
-		myv.y = r*Math.sin(myv.z/30);
-		// myv.y = 10*noise.simplex3(myv.x/10, myv.z/10,0);
-	}
-	geometry.verticesNeedUpdate = true;
-	geometry.computeFaceNormals();
-	geometry.normalsNeedUpdate = true;
-
-	// geometry.vertices.push(new THREE.Vector3(50, -50, 0));
-	// geometry.vertices.push(new THREE.Vector3(-50, 50, 0));
-	// geometry.vertices.push(new THREE.Vector3(-50, -50, -30));
-	// geometry.vertices.push(new THREE.Vector3(50, 50, -30));
-
-	// // geometry.faces.push(new THREE.Face3(0, 1, 2, null, new THREE.Color(0.9, 0.7, 0.75)));
-	// // geometry.faces.push(new THREE.Face3(0, 3, 1, null, new THREE.Color(0.6, 0.85, 0.7)));
-
-	// var colors = [new THREE.Color(1, 0, 0), new THREE.Color(0, 1, 0), new THREE.Color(0, 0, 1)];
-	// geometry.faces.push(new THREE.Face3(0, 1, 2, null, colors));
-	// geometry.faces.push(new THREE.Face3(0, 3, 1, null, colors));
-
-	// geometry.computeFaceNormals();
-	// geometry.computeVertexNormals();
-	// geometry.normalsNeedUpdate = true;
+	let myEntity = new PolyCurtain({x:0, y:0, z:0}, 20, 10);
+	myEntity.init();
+	entities.push(myEntity);
 	
+
+	// let width = 10;
+	// let height = 20;
+	// let indices = [];
+	// let index = 0;
+
+	// let t_spacing = 10;
+	// let t_width = t_spacing * width;
+	// let t_left = -0.5*t_width;
+	// let t_height = t_spacing * height;
+	// let t_bottom = -0.5*t_height;
+
+	// //construct vertices
+	// for(i=0; i<width; i++) {
+	// 	let row = [];
+	// 	for(j=0; j<height; j++) {
+	// 		geometry.vertices.push(new THREE.Vector3(i*t_spacing+t_left,0,j*t_spacing+t_bottom));
+	// 		row.push(index);
+	// 		index++;
+	// 	}
+	// 	indices.push(row);
+	// }
+	// //construct faces
+	// for(i=0; i<width-1; i++) {
+	// 	for(j=0; j<height-1; j++) {
+	// 		geometry.faces.push(new THREE.Face3(indices[i+1][j+1], indices[i+1][j], indices[i][j]));
+	// 		geometry.faces.push(new THREE.Face3(indices[i][j+1], indices[i+1][j+1], indices[i][j]));
+	// 	}
+	// }
+
+	// for(i=0; i<geometry.vertices.length; i++) {
+	// 	let myv = geometry.vertices[i];
+	// 	// myv.x += -5+Math.random()*10;
+	// 	// myv.z += -5+Math.random()*10;
+	// 	let r = myv.x;
+	// 	myv.x = r*Math.cos(myv.z/30);
+	// 	myv.y = r*Math.sin(myv.z/30);
+	// 	// myv.y = 10*noise.simplex3(myv.x/10, myv.z/10,0);
+	// }
+	// geometry.verticesNeedUpdate = true;
+	// geometry.computeFaceNormals();
+	// geometry.normalsNeedUpdate = true;
 
 	//finish
 	document.getElementById("WebGL-output").appendChild(renderer.domElement);
