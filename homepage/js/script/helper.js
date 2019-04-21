22d9bcd5a1d161a446f140ba2622d1669ed7980c
Alan Luo <me@alanluo.com>
2019-04-20 22:13:26 -0400

    cleaned helper.js

--- homepage/js/script/helper.js
+++ homepage/js/script/helper.js
@@ -25,41 +25,7 @@ var createMultiMaterialObject = function ( geometry, materials ) {
 	return group;
 }
 
-// an object that creates a continuous color mapping of any real number onto a color
-function ColorMap(startColor, endColor) {
-	this.startColor = {
-		h: startColor.h, 
-		s: startColor.s, 
-		l: startColor.l,
-	};
-	this.endColor = {
-		h: endColor.h, 
-		s: endColor.s, 
-		l: endColor.l,
-	};
-	this.delColor = {
-		h: endColor.h-startColor.h, 
-		s: endColor.s-startColor.s, 
-		l: endColor.l-startColor.l,
-	};
-	this.getColor = function(percent) {
-		var progress = ((percent % 2)+2)%2; // get positive mod
-		if(progress<=1) { 
-			return {
-				h: this.startColor.h + progress * this.delColor.h,
-				s: this.startColor.s + progress * this.delColor.s,
-				l: this.startColor.l + progress * this.delColor.l,
-			};
-		} else {
-			progress -= 1;
-			return {
-				h: this.endColor.h - progress * this.delColor.h,
-				s: this.endColor.s - progress * this.delColor.s,
-				l: this.endColor.l - progress * this.delColor.l,
-			};
-		}
-	}
-}
+// this is simply a cross-browser compatible function for requesting full-screen
 function requestFullscreen() {
 	var el = renderer.domElement;
 
@@ -77,7 +43,7 @@ function requestFullscreen() {
 	}
 }
 
-
+// another helper function for VR
 function onFullscreenChange(e) {
 	var fsElement = document.fullscreenElement ||
 	document.mozFullScreenElement ||
@@ -91,6 +57,7 @@ function onFullscreenChange(e) {
 	}
 }
 
+// self-explanatory, checks if browser is mobile
 var isMobile = function () {
 	var check = false;
 	(function (a) {
@@ -100,6 +67,8 @@ var isMobile = function () {
 	})(navigator.userAgent || navigator.vendor || window.opera);
 	return check;
 };
+
+// adds a warning message of someone clicks the VR button using a desktop browser
 document.querySelector('#enterVr').addEventListener('click', function () {
 	if (!is_mobile) {
 		alert('You do not have a WebVR capable device. Consider getting a Google Cardboard.')
@@ -110,6 +79,7 @@ document.querySelector('#enterVr').addEventListener('click', function () {
 	onWindowResize();
 });
 
+// ensures that the WebGL viewport is always the same size as the browser window
 function onResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
